668042c66883fba45fb5a722e030b07acfb51a86
Melon Bao <melon@Melons-MacBook-Pro.local>
2020-02-23 21:49:07 +0800

    enable spring mointor

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,19 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.example.demo.EmployeeMapper;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Autowired
     EmployeeController controller;
 
-    @Test
-    void contextLoads() {
-    }
 
     @Test
     void testController(){
         Employee employee = controller.getEmployeeById(1);
+        assertEquals("test name:", "alex",employee.getName());
         System.out.println("get employee =>"+ employee);
     }
 }
